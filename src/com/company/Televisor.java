fd798458d59e4b37cfbbe2b5f797068b67063d3c
LeszekS <leszekstepien@o2.pl>
2019-01-18 13:22:34 +0100

    Zadanie 4.1`

---
+++ src/com/company/Televisor.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import static java.awt.SystemColor.info;
+
+public class Televisor {
+
+    private boolean switchOffOn;
+
+    public boolean isSwitchOffOn() {
+        return switchOffOn;
+    }
+
+    public void setSwitchOffOn(boolean switchOffOn) {
+        this.switchOffOn = switchOffOn;
+    }
+
+    void turnOn() {
+        setSwitchOffOn(true);
+    }
+
+    ;
+
+    void turnOff() {
+        setSwitchOffOn(false);
+    }
+
+    ;
+
+    public String showStatus() {
+        String info;
+        if (switchOffOn) {
+            info = "Włączony";
+        } else {
+            info = "Wyłączony";
+        }
+        ;
+        return "Televisor {" + info +'}';
+    }
+}
