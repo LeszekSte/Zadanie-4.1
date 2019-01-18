ca3bca167e686a068c3286bc0106755e30fcb862
LeszekS <leszekstepien@o2.pl>
2019-01-18 13:23:48 +0100

    Zadanie 4.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Televisor televisor = new Televisor( );
+
+        System.out.println(televisor.showStatus());
+	    televisor.turnOn();
+        System.out.println(televisor.showStatus());
+	    televisor.turnOff();
+        System.out.println(televisor.showStatus());
+    }
+}
